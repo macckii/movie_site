ff8367a5f8bbe3053fc4a0ee4107c8de18e491d0
Maki Sugita <macckii@hotmail.com>
2015-07-25 18:36:08 +1000

    authentication

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ var mongo = require('mongodb');
 var monk  = require('monk');
 var db    = monk('localhost:27017/movie_site');
 
+// aithentication
+var mongoose      = require('mongoose');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 // routes
 var home       = require('./routes/index');
 var references = require('./routes/references');
@@ -33,6 +38,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make db accessible to router
@@ -44,6 +56,15 @@ app.use(function(req, res, next){
 app.use('/', home);
 app.use('/references', references);
 app.use('/users', users);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/user')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
